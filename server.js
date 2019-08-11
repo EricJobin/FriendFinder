18838550ebc762ba35f1fe9d88794fdabe4845c9
Eric Jobin <ejobin@gmail.com>
2019-08-11 15:56:06 -0400

    Have server.js listening and serving home.html & survey.html

--- server.js
+++ server.js
@@ -11,5 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
