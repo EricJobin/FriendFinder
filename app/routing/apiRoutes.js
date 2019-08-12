8e4cf62148a86a466e12c86b8c055057b57e2fd7
Eric Jobin <ejobin@gmail.com>
2019-08-12 09:57:16 -0400

    Figured out why none of my apiRoutes worked, didn't include them on server.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,17 +3,6 @@
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-// var tableData = require("../data/tableData");
-// var waitListData = require("../data/waitinglistData");
-
-// require("./app/routing/htmlRoutes")(app);
 console.log("apiRoutes.js checking in")
 var friendsArray = require("../data/friends");
 
@@ -22,23 +11,10 @@ var friendsArray = require("../data/friends");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-    // app.get("/api/survey", function(req, res) {
-    //     res.json(friendsArray);
-    // });
 
     app.get("/api/survey", function(req, res) {
         return res.json(friendsArray);
     });
-
-    // app.get("/api/waitlist", function(req, res) {
-    //     res.json(waitListData);
-    // });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 
 
 //Activate Server
