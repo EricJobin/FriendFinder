907cd82a7dae0411ba56b373246fc0cc3053cc3e
Eric Jobin <ejobin@gmail.com>
2019-08-13 10:30:41 -0400

    To see pictures hosted on server, you need to add a line to server.js to serve the files

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("app/data"))// this allows the server to serve images hosted in the app/data(/images) folder
 app.use(express.json());
 
 require("./app/routing/apiRoutes")(app); // Important to include this line, otherwise stuff doesn't work. Also need to put it before the bottom one where you've got the catch all * route
